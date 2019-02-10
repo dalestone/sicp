79431ba7456f71c63f66c69f7b119d4bd917bdd4
Dale Stone <dale@stonenet.co.uk>
2019-02-10 17:44:00 +0000

    added imcomplete solution to 1.11

--- 1/1.10.scm
+++ 1/1.10.scm
@@ -11,5 +11,6 @@
 
 (define (f n) (A 0 n)) ; Mathematical definition: 2n
 (define (g n) (A 1 n)) ; Mathematical definition: 2^n
-(define (h n) (A 2 n)) ; I have yet be able to solve this, the closest to a solution was 2^(2^(n-1)(n/2)) but this results in h(1) being incorrect
+(define (h n) (A 2 n)) ; I have yet to solve this, the closest to a solution was 2^(2^(n-1)(n/2)) but this results in h(1) being incorrect
+    ; I'm probably missing something obvious...
 (define (k n) (* 5 n n)) ; Mathematical definition: 5n^2
--- 1/1.11.scm
+++ 1/1.11.scm
@@ -1,5 +1,17 @@
+; Recursive
 (define (f n)
   (cond ((< n 3) n)
         ((>= n 3) (+ (f(- n 1)) (* 2 (f(- n 2))) (* 3 (f(- n 3)))))))
 
-(define (f-inter n))
+; Iterative - not fully implemented as I gave up
+(define (f-result a b c)
+  (+ a (* 2 b) (* 3 c)))
+
+(define (f-iter n count result)
+  (if (= count (- n 2))
+    result
+      (f-iter n (+ count 1) result)))
+
+(define (f n)
+  (cond ((< n 3) n)
+        (f-iter n 0 (f-result 2 1 0))))
